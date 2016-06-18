9cdafa7298718289476a1944c1b42f5008770d41
haretaro <frontend.1000@gmail.com>
2016-06-18 09:48:52 +0000

    change

--- readkey.py
+++ readkey.py
@@ -1,16 +1,37 @@
 import tornado.web
 import tornado.websocket
 from tornado.ioloop import PeriodicCallback
+import RPi.GPIO as GPIO
+
+right_driver_forward = 27
+right_driver_backward = 17
+left_driver_forward = 15
+left_driver_backward = 14
+threshold = 30
+
+gpios = [right_driver_forward,
+        right_driver_backward,
+        left_driver_forward,
+        left_driver_backward]
+
+print(gpios)
+GPIO.setmode(GPIO.BCM)
+for pin in gpios:
+    print(pin)
+    GPIO.setup(pin, GPIO.OUT)
+
+ly = 0
+ry = 0
 
 class MainPage(tornado.web.RequestHandler):
 
     def get(self):
-        self.render('keyread.html')
+        self.render('readkey.html')
 
 class SendWebSocket(tornado.websocket.WebSocketHandler):
 
     def on_message(self, message):
-        print(chr(int(message)))
+        print(chr(int(message)),message,int(message)==ord('A'))
 
 app = tornado.web.Application([
     (r'/',MainPage),
